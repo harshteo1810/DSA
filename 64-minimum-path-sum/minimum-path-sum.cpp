e175a592ce2915bcbccb4ab0275c3914f31778e2
harshteo1810 <harshteo@gmail.com>
2026-07-30 12:22:01 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (42.96%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -17,25 +17,25 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        // dp[0][0] = grid[0][0];
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         if(i==0 && j==0){
-        //             continue;
-        //         }
-        //         int down = 1e9;
-        //         int right = 1e9;
-        //         if(i!=0){
-        //             down  = grid[i][j] + dp[i-1][j];
-        //         }
-        //         if(j!=0){
-        //             right = grid[i][j] + dp[i][j-1];
-        //         }
-        //         dp[i][j] = min(down,right);
-        //     }
-        // }
-        // return dp[n-1][m-1];
-        return func(n-1,m-1,n,m,grid,dp);
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        dp[0][0] = grid[0][0];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    continue;
+                }
+                int down = 1e9;
+                int right = 1e9;
+                if(i!=0){
+                    down  = dp[i-1][j];
+                }
+                if(j!=0){
+                    right = dp[i][j-1];
+                }
+                dp[i][j] = grid[i][j] + min(down,right);
+            }
+        }
+        return dp[n-1][m-1];
+        // return func(n-1,m-1,n,m,grid,dp);
     }
 };
