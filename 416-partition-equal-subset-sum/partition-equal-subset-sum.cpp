0f6fe9ef322b46fbebc46c7a07be59e322d0329b
harshteo1810 <harshteo@gmail.com>
2026-08-01 12:23:00 +0530

    Time: 112 ms (67.85%) | Memory: 110.3 MB (45.54%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool func(int i,int tar,vector<int>&nums,vector<vector<int>> &dp){
-        // if(tar<0 || i<0){
-        //     return false;
-        // }
         if(tar==0){
             return true;
         }
@@ -30,7 +27,24 @@ public:
             return false;
         }
         int val = totalSum/2;
-        vector<vector<int>> dp(n,vector<int>(val+1,-1));
-        return func(n-1,val,nums,dp);
+        vector<vector<int>> dp(n,vector<int>(val+1,0));
+        if(nums[0]<=val){
+            dp[0][nums[0]] = 1;
+        }
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=val;j++){
+                int non = dp[i-1][j];
+                int pick = 0;
+                if(nums[i]<=j){
+                    pick = dp[i-1][j-nums[i]];
+                }
+                dp[i][j] = pick || non;
+            }
+        }
+        return dp[n-1][val];
+        // return func(n-1,val,nums,dp);
     }
 };
