eb4b7671174884e1275654a3a6c92fed65bfee87
harshteo1810 <harshteo@gmail.com>
2026-08-27 17:28:32 +0530

    Time: 7 ms (14.98%) | Memory: 17.9 MB (91.60%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -5,16 +5,18 @@ public:
         for(int i:nums){
             mpp[i]++;
         }
-        vector<pair<int,int>> ans;
-        for(auto &p:mpp){
-            ans.push_back({p.first,p.second});
-        }
-        sort(ans.begin(),ans.end(),[](const auto& a, const auto& b) {
-            return a.second >b.second;
-        });
         vector<int> res;
         for(int i=0;i<k;i++){
-            res.push_back(ans[i].first);
+            int maxi = 0;
+            int ele=0;
+            for(auto &p:mpp){
+                if(p.second>maxi){
+                    maxi = p.second;
+                    ele = p.first;
+                }
+            }
+            mpp.erase(ele);
+            res.push_back(ele);
         }
         return res;
     }
