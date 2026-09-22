35109725c24094294bcad894c601b35c1ff113f7
harshteo1810 <harshteo@gmail.com>
2026-09-22 11:24:38 +0530

    Time: 3 ms (68.72%) | Memory: 64.9 MB (99.56%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1]){
-                return nums[i];
-            }
+        // slow fast pointer technique of LL
+        int slow = nums[0]; // initialise 
+        int fast = nums[0]; // initialise
+
+        //used do while because we don't want to encounter slow==fast at starting
+        do{
+            slow = nums[slow]; //increment by 1 pointer
+            fast = nums[nums[fast]]; // increment by 2 pointer
         }
-        return 0;
+        while(slow!=fast);
+        // once slow == fast occured we reinitialize slow to initial index value
+        slow = nums[0];
+        while(slow!=fast){
+            slow = nums[slow]; //increment by 1 pointer
+            fast = nums[fast]; //increment by 1 pointer
+        }
+        return slow;
     }
 };
